adac32cb22fcdd3213055856c9572df722f50752
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-02-18 19:21:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sugar = sc.nextInt();
+        int bag = 0;
+
+        while(sugar >= 0) {
+            if(sugar % 5 ==0) {
+                bag += sugar / 5;
+                sugar = 0;
+                break;
+            }
+            sugar -= 3;
+            bag += 1;
+        }
+
+        if(sugar == 0) System.out.println(bag);
+        else System.out.println(-1);
+    }
+}
