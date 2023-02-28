3ab40c30988d7f665f25d54814556c30f067f791
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-02-28 15:32:08 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 21620 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,18 @@
 import java.util.*;
 public class Main {
-    static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(solution(n));
     }
-
-    private static int solution(int number) {
-        dp = new int[number+1];
-        dp[0] = 0;
+    private static int solution(int n) {
+        int[] dp = new int[n+1];
         dp[1] = 0;
-        for(int i=2; i<=number; i++) {
+        for(int i=2; i<=n; i++) {
             dp[i] = dp[i-1] + 1;
-            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
-            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if(i % 3 ==0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
+            if(i % 2 ==0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
         }
-        return dp[number];
+        return dp[n];
     }
 }
