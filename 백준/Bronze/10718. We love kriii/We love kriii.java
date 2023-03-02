5546bb14aebbd481dd2b3f1a5b9e9fa5a8fb3786
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-03-02 21:07:27 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(new StringBuilder().append("강한친구 대한육군").append("\n").append("강한친구 대한육군"));
+    }
+}
