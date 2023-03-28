da6d00e81dff3bfc7db776a932c43a414bf1585c
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-03-28 10:20:39 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.61 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int b_len = B.length-1;
+        for(int i=0; i<A.length; i++) {
+            answer += (A[i] * B[b_len]);
+            b_len--;
+        }
+        return answer;
+    }
+}
