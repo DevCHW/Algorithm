2010671f09a7705e3fb0007129f9bc43a639064c
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-03 11:10:06 +0900

    [level 2] Title: 구명보트, Time: 9.17 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        //오름차순 정렬
+        Arrays.sort(people);
+        
+        //몸무게가 가장 낮은사람 + 가장 높은사람 <= limit 이라면 같이 탑승,
+        //아니라면 가장 높은사람 혼자 탑승
+        
+        int lt = 0;
+        for(int rt=people.length-1; rt>=lt; rt--) {
+            if(people[lt] + people[rt] <= limit) lt++;
+            answer++;
+        }
+        return answer;
+    }
+}
