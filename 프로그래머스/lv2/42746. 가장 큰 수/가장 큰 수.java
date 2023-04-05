97adce62b69b675b24e92cc66fcc0bec0e549fab
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-05 10:07:58 +0900

    [level 2] Title: 가장 큰 수, Time: 2.54 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] arr = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++) arr[i] = String.valueOf(numbers[i]);
+        
+        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
+        if(arr[0].equals("0")) answer = "0";
+        else {
+            StringBuilder sb = new StringBuilder();
+            for(String x : arr) sb.append(x);
+            answer = sb.toString();
+        }
+        
+        return answer;
+    }
+}
