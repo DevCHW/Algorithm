c8ac2e8149103db91d3e022380f7a5767ef76f05
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-07 11:22:02 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.69 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        // m*n n*k 행렬의 곱셈 결과는 m*k 행렬이다. 
+        // 결과의 배열 생성
+        
+        for(int i = 0; i < answer.length; i++){ 
+            for(int j = 0; j < answer[i].length; j++){
+                for(int k = 0; k < arr1[0].length; k++){
+                    answer[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
