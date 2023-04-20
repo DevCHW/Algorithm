56e5dd297804eb5852665a077c149ec8a819587b
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-21 05:29:01 +0900

    [Gold V] Title: 적록색약, Time: 236 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,97 +1,74 @@
-import java.util.*;
-class Position {
-    int x;
-    int y;
-    Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.util.Scanner;
+
 public class Main {
+    static char[][] arr;
+    static int[][] visited; //일반인 방문배열
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int n;
-    static char[][] image;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        image = new char[n][n];
+        arr = new char[n][n];
+        visited = new int[n][n];
+
         for(int i=0; i<n; i++) {
             String str = sc.next();
-            int j=0;
-            for(char x : str.toCharArray()) {
-                image[i][j] = x;
-                j++;
+            for(int j=0; j<n; j++) {
+                arr[i][j] = str.charAt(j);
             }
         }
-        System.out.println(solution());
+        solution(n, arr);
     }
 
-    private static String solution() {
-        int[][] visited = new int[n][n];
-        int nomalCnt = 0;
-        int specialCnt = 0;
-        boolean flag = false;
-        //일반사람 카운트하기
+    private static void solution(int n, char[][] arr) {
+        // 적록색약 -> 빨간색과 초록색 같음
+        // 일반인 -> 다 다름
+
+        int count1 = 0;
+        int count2 = 0;
+        //일반인 구역의 수 구하기
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(visited[i][j] == 0) {
-                    nomalCnt++;
-                    bfs(i, j, visited, image[i][j],flag);
+                    count1++;
+                    dfs(i, j, arr[i][j]);
                 }
             }
         }
 
-        String answer = nomalCnt + " ";
-        flag = true;
-        for(int[] x : visited) {
-            Arrays.fill(x,0);
+        // 적록색약의 구역 수를 구하기 위하여 'R'을 'G'로 전부 바꿈
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(arr[i][j] == 'R') {
+                    arr[i][j] = 'G';
+                }
+                visited[i][j] = 0;
+            }
         }
-        //적록색약 카운트하기
+
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(visited[i][j] == 0) {
-                    specialCnt++;
-                    bfs(i, j, visited, image[i][j], flag);
+                    count2++;
+                    dfs(i, j, arr[i][j]);
                 }
             }
         }
-        answer += specialCnt;
-        return answer;
+
+        System.out.println(count1);
+        System.out.println(count2);
     }
 
-    private static void bfs(int x, int y, int[][] visited, char tmp, boolean flag) {
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-        Queue<Position> queue = new LinkedList<>();
-        queue.offer(new Position(x, y));
-        visited[x][y] = 1;
-
-        while(!queue.isEmpty()) {
-            Position p = queue.poll();
-            for(int i=0; i<4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-
-                if(!flag) {
-                    if(0<= nx && nx <n && 0<= ny && ny < n && visited[nx][ny] == 0 && image[nx][ny] == tmp) {
-                        visited[nx][ny] = 1;
-                        queue.offer(new Position(nx, ny));
-                    }
-                } else {    //적록색약 따지기
-                    if(tmp == 'R' || tmp == 'G') {  //R이나 G가 들어왔다면
-                        if(0<= nx && nx <n && 0<= ny && ny < n && visited[nx][ny] == 0 && (image[nx][ny] == 'R' || image[nx][ny] == 'G')) {
-                            visited[nx][ny] = 1;
-                            queue.offer(new Position(nx, ny));
-                        }
-                    } else {
-                        if(0<= nx && nx <n && 0<= ny && ny < n && visited[nx][ny] == 0 && image[nx][ny] == tmp) {
-                            visited[nx][ny] = 1;
-                            queue.offer(new Position(nx, ny));
-                        }
-                    }
-                }
-
-            }//end of for--
-        }//end of while--
+    private static void dfs(int x, int y, char color) {
+        visited[x][y] = 1; //방문처리
+        for(int i=0; i<4; i++) {
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx >= 0 && nx < n && ny >= 0 && ny < n && visited[nx][ny] == 0 && arr[nx][ny] == color) {
+                dfs(nx, ny, color);
+            }
+        }
     }
-
 }
