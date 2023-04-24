45bd18fa73602204e0c65b90ef35a4655ac6bc59
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-24 10:56:24 +0900

    [unrated] Title: 숫자 변환하기, Time: 11.89 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int x, int y, int n) {
+        int[] dp = new int[y+1];
+        for(int i=x; i<=y; i++) {
+            if(i != x && dp[i] == 0) { //만들 수 없는 수는 전부 -1
+                dp[i] = -1;
+                continue;
+            }
+            if(i+n <= y) dp[i+n] = dp[i+n] == 0? dp[i]+1:Math.min(dp[i]+1, dp[i+n]);
+            if(i*2 <= y) dp[i*2] = dp[i*2] == 0? dp[i]+1:Math.min(dp[i]+1, dp[i*2]);
+            if(i*3 <= y) dp[i*3] = dp[i*3] == 0? dp[i]+1:Math.min(dp[i]+1, dp[i*3]);
+        }
+        int answer = dp[y];
+        return answer;
+    }
+    
+    
+}
