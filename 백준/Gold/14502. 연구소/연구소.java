36bc21fa31bf6cf05dbb068ca0404716601a5b8f
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-04-28 00:36:13 +0900

    [Gold IV] Title: 연구소, Time: 500 ms, Memory: 140300 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,99 +1,99 @@
 import java.util.*;
-class Position {
-    int x;
-    int y;
-    Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
+    static class Position {
+        int x;
+        int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     static int n;
     static int m;
 
-    static int[][] map;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-
-        map = new int[n][m];
-
+        n = sc.nextInt();   //세로
+        m = sc.nextInt();   //가로
+        int[][] graph = new int[n][m];
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                map[i][j] = sc.nextInt();
+                graph[i][j] = sc.nextInt();
             }
         }
-        System.out.println(solution());
+        System.out.println(solution(graph));
     }
-    private static int solution() {
-        List<Position> zeroIndex = new ArrayList<>();
+    private static int solution(int[][] graph) {
         int answer = Integer.MIN_VALUE;
-
+        List<Position> zeroIndexList = new ArrayList<>();  //0인 위치를 기록할 List 선언
+        Queue<Position> virusIndex = new LinkedList<>();   //바이러스의 위치를 기록할 queue
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(map[i][j]== 0) zeroIndex.add(new Position(i, j));
+                if (graph[i][j] == 0) { //빈공간이라면 기록
+                    zeroIndexList.add(new Position(i, j));
+                }
+                if (graph[i][j] == 2) { //바이러스라면 기록
+                    virusIndex.offer(new Position(i, j));
+                }
             }
         }
+        // 모든 조합수에 대하여 일단 3개 벽을 세워보기. (3중 for문으로 따지기)
+        int len = zeroIndexList.size();
+        for(int i=0; i<len; i++) {
+            for(int j=i+1; j<len; j++) {
+                for(int k=j+1; k<len; k++) {
+                    //벽 세울 위치 3개 뽑기
+                    Position wall1 = zeroIndexList.get(i);
+                    Position wall2 = zeroIndexList.get(j);
+                    Position wall3 = zeroIndexList.get(k);
 
-        //0의 위치중 아무거나 3개 뽑아서 벽으로 만들기(중복은안됨)
-        int size = zeroIndex.size();
-        for(int i=0; i<size; i++) {
-            for(int j=i+1; j<size; j++) {
-                for(int k=j+1; k<size; k++) {
-                    int x1 = zeroIndex.get(i).x;
-                    int y1 = zeroIndex.get(i).y;
-                    int x2 = zeroIndex.get(j).x;
-                    int y2 = zeroIndex.get(j).y;
-                    int x3 = zeroIndex.get(k).x;
-                    int y3 = zeroIndex.get(k).y;
+                    //벽 세우기
+                    graph[wall1.x][wall1.y] = 1;
+                    graph[wall2.x][wall2.y] = 1;
+                    graph[wall3.x][wall3.y] = 1;
 
-                    map[x1][y1] = 1;    //임의로 3군데 벽 세워보기
-                    map[x2][y2] = 1;
-                    map[x3][y3] = 1;
+                    //bfs로 바이러스 퍼트리기
+                    int[][] visited = new int[n][m]; //방문배열 만들기
+                    answer = Math.max(answer, bfs(graph, visited, virusIndex)); //바이러스 퍼트려보고 안전영역 맥스값 갱신
 
-                    int[][] visited = new int[n][m];
-                    for(int x=0; x<n; x++) {
-                        for(int y=0; y<m; y++) {
-                            //map을 탐색해서 바이러스를 만난다면 bfs로 0인곳에 바이러스 전파.
-                            //visited 배열로 퍼트린 곳 체크.
-                            if(map[x][y] == 2) bfs(x, y, visited);
-                        }
-                    }
-                    int count = 0;  // 안전 영역 갯수 세기
-                    for(int x=0; x<n; x++) {
-                        for(int y=0; y<m; y++) {
-                            if(map[x][y] == 0 && visited[x][y] == 0) count++;
-                        }
-                    }
-                    answer = Math.max(count, answer);
-
-                    map[x1][y1] = 0;    //벽으로 세워놨던 곳 다시 돌려놓기
-                    map[x2][y2] = 0;
-                    map[x3][y3] = 0;
+                    //벽으로 만들었던 것 원상복구 시키기.
+                    graph[wall1.x][wall1.y] = 0;
+                    graph[wall2.x][wall2.y] = 0;
+                    graph[wall3.x][wall3.y] = 0;
                 }
             }
         }
         return answer;
     }
 
-    private static void bfs(int x, int y, int[][] visited) {
+    private static int bfs(int[][] graph, int[][] visited, Queue<Position> virusIndex) {
         Queue<Position> queue = new LinkedList<>();
-        queue.offer(new Position(x, y));
-        visited[x][y] = 1;  //바이러스가 있는곳 방문처리
-        while(!queue.isEmpty()) {   //바이러스 퍼트리기
-            Position p = queue.poll();
+        for(Position p : virusIndex) queue.offer(p);  //바이러스 위치 복사
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while(!queue.isEmpty()) {
+            Position pos = queue.poll();
             for(int i=0; i<4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if(0<= nx && nx < n && 0<= ny && ny < m && map[nx][ny] == 0 && visited[nx][ny] == 0) {
-                    visited[nx][ny] = 1;
+                int nx = pos.x + dx[i];
+                int ny = pos.y + dy[i];
+                if(nx >= 0 && nx < n && ny >= 0 && ny< m && graph[nx][ny] == 0 && visited[nx][ny] == 0) {   //graph 상에서 빈공간(0)이고 방문하지 않은곳이라면,
                     queue.offer(new Position(nx, ny));
+                    visited[nx][ny] = 1;    //방문 처리
                 }
             }
-        }//end of while--
+        }
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(graph[i][j] == 0 && visited[i][j] == 0) {    //graph 상에서 안전영역(0)이고, 바이러스가 퍼지지 않은 곳이라면
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
