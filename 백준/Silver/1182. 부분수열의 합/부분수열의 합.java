01ac4fb4c08657a0e3b3fda542ef5013f08cb90d
HyunWoo Choi <112748454+DevCHW@users.noreply.github.com>
2023-07-11 19:37:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,34 +1,51 @@
 import java.util.*;
-public class Main {
-    static int answer;
-    static int[] visited;
 
+public class Main {
+
+    static int answer;
+
+    /**
+     * 입,출력
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int s = sc.nextInt();
-
         int[] arr = new int[n];
         for(int i=0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
-
         solution(n, s, arr);
-        System.out.println(answer);
+        print(answer);
     }
 
     private static void solution(int n, int s, int[] arr) {
-        visited = new int[n];
-        recursive(0,0, s, arr);
-        if(s == 0) answer-=1;
+        recursive(0, n, s, arr, 0);
+        if(s == 0) answer--;
     }
 
-    private static void recursive(int depth, int sum, int s, int[] arr) {
-        if(depth == arr.length) {
-            if(sum == s) answer++;
-            return;
+    private static void recursive(int depth, int n, int s, int[] arr, int number) {
+        if (depth == n) {
+            if(number == s) {
+                answer++;
+            }
+        } else {
+            recursive(depth+1, n, s, arr, number+arr[depth]);
+            recursive(depth+1, n, s, arr, number);
         }
-        recursive(depth + 1, sum + arr[depth], s, arr);
-        recursive(depth + 1, sum, s, arr);
+    }
+
+    /**
+     * print() 구현
+     */
+    private static void print(String str) {
+        System.out.println(str);
+    }
+
+    /**
+     * print() 구현
+     */
+    private static void print(int number) {
+        System.out.println(number);
     }
 }
